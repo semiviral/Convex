3ee582d1ba3ae90b5a00834074799ca5b9420350
Antonio DiNostri <semiviral@gmail.com>
2018-10-12 03:24:48 -0500

    SignalR configuration

---
+++ Convex.Net/Hubs/IrcHub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Convex.Client.Hubs
+{
+    public class IrcHub : Hub
+    {public async Task SendMessage(String udser,)
+    }
+}
--- Convex.Net/Startup.cs
+++ Convex.Net/Startup.cs
@@ -1,6 +1,8 @@
-﻿using Convex.Client.Services;
+﻿using Convex.Client.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -18,23 +20,30 @@ namespace Convex.Client {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddMvc();
-            //services.AddSingleto, ClientHostedService>(clientHostedService => new ClientHostedService("irc.foonetic.net", 6667));
+            services.Configure<CookiePolicyOptions>(options => {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
+            } else {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
-            app.UseMvc(routes => {
-                routes.MapRoute(
-                    "default",
-                    "{controller=Root}/{action=Index}/{id?}");
-            });
+            app.UseSignalR(routes => { routes.MapHub<IrcHub>("/ircHub"); });
+            app.UseMvc();
         }
     }
 }
